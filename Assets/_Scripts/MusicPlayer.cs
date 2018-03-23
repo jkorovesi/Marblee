44ed2468478c76ba118ac61885b2404dd75c8d29
jkorovesi <joshua.j.korovesi@hotmail.com>
2018-03-22 23:58:27 -0400

    Added pause, music, and tidied up a bit
    - Deleted some unused folders and scenes
    - Added music
    - Added a pause menu

---
+++ Assets/_Scripts/MusicPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+
+    static MusicPlayer instance = null;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if(SceneManager.GetActiveScene().name == "MainMenu")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
